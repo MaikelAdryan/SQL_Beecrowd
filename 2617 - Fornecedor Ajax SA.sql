3b7fde0da892b880a692bd7ed13f7ac8d94d8204
adryanmaikel <adryanmaikel@gmail.com>
2023-10-27 09:12:58 -0300

    criando enunciado

---
+++ 2617 - Fornecedor Ajax SA.sql
@@ -0,0 +1,24 @@
+/*
+O setor financeiro encontrou alguns problemas na entrega de um dos nossos fornecedores,
+a entrega dos produtos não condiz com a nota fiscal.
+
+Seu trabalho é exibir o nome dos produtos e o nome do fornecedor, para os produtos fornecidos
+pelo fornecedor ‘Ajax SA’.
+
+| id  | name         | street                    | city           | state |
+| --- | ------------ | ------------------------- | -------------- | ----- |
+| 1   | Ajax SA      | Presidente Castelo Branco | Porto Alegre   | RS    |
+| 2   | Sansul SA    | Av Brasil                 | Rio de Janeiro | RJ    |
+| 3   | South Chairs | Av Moinho                 | Santa Maria    | RS    |
+| 4   | Elon Electro | Apolo                     | São Paulo      | SP    |
+| 5   | Mike Electro | Pedro da Cunha            | Curitiba       | PR    |
+
+| id  | name             | amount | value   | id_providers |
+| --- | ---------------- | ------ | ------- | ------------ |
+| 1   | Blue Chair       | 30     | 300.00  | 5            |
+| 2   | Red Chair        | 50     | 2150.00 | 1            |
+| 3   | Disney Wardrobe  | 400    | 829.50  | 4            |
+| 4   | Blue Toaster     | 20     | 9.90    | 3            |
+| 5   | Solar Panel      | 30     | 3000.25 | 4            |
+
+*/
