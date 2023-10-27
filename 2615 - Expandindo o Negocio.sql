255c100a15d8f08dbdd46f2b433264f441c1eff1
adryanmaikel <adryanmaikel@gmail.com>
2023-10-27 08:38:04 -0300

    adicionando enunciado

---
+++ 2615 - Expandindo o Negocio.sql
@@ -0,0 +1,19 @@
+/*
+A locadora tem objetivos de criar várias franquias espalhadas pelo Brasil. Para isso queremos saber
+em quais cidades nossos clientes moram.
+
+Para você nos ajudar selecione o nome de todas as cidades onde a locadora tem clientes. Mas por favor,
+não repita o nome da cidade.
+
+
+| id  | name                        | street                         | city            |
+| --- | --------------------------- | ------------------------------ | --------------- |
+| 1   | Giovanna Goncalves Oliveira | Rua Mato Grosso                | Canoas          |
+| 2   | Kauã Azevedo Ribeiro        | Travessa Ibiá                  | Uberlândia      |
+| 3   | Rebeca Barbosa Santos       | Rua Observatório Meteorológico | Salvador        |
+| 4   | Sarah Carvalho Correia      | Rua Antônio Carlos da Silva    | Uberlândia      |
+| 5   | João Almeida Lima           | Rua Rio Taiuva                 | Ponta Grossa    |
+| 6   | Diogo Melo Dias             | Rua Duzentos e Cinqüenta       | Várzea Grande   |
+
+*/
+
