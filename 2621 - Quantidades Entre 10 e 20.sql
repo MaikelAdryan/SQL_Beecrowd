d9f68d10e594f082086b87d914191d4fb391569e
adryanmaikel <adryanmaikel@gmail.com>
2023-10-27 10:10:54 -0300

    criando atividade

---
+++ 2621 - Quantidades Entre 10 e 20.sql
@@ -0,0 +1,29 @@
+/* Quantidades Entre 10 e 20
+Na hora de entregar o relatório de quantos produtos a empresa tem em estoque,
+uma parte do relatório ficou corrompida, por isso o responsável do estoque lhe
+pediu uma ajuda, ele quer que você exiba os seguintes dados para ele.
+
+Exiba o nome dos produtos cujas quantidades estejam entre 10 e 20 e cujo nome
+do fornecedor inicie com a letra ‘P’.
+
+## Tabelas
+
+### Tabela "providers":
+| id  | name                 | street                        | city          | state |
+| --- | -------------------- | ----------------------------  | ------------- | ----- |
+| 1   | Ajax SA              | Rua Presidente Castelo Branco | Porto Alegre  | RS    |
+| 2   | Sansul SA            | Av Brasil                     | Rio de Janeiro| RJ    |
+| 3   | Pr Sheppard Chairs   | Rua do Moinho                 | Santa Maria   | RS    |
+| 4   | Elon Electro         | Rua Apolo                     | São Paulo     | SP    |
+| 5   | Mike Electro         | Rua Pedro da Cunha            | Curitiba      | PR    |
+
+### Tabela "products":
+| id  | name              | amount | price   | id_providers |
+| --- | ----------------- | ------ | ------- | ------------ |
+| 1   | Blue Chair        | 30     | 300.00  | 5            |
+| 2   | Red Chair         | 50     | 2150.00 | 2            |
+| 3   | Disney Wardrobe   | 400    | 829.50  | 4            |
+| 4   | Executive Chair   | 17     | 9.90    | 3            |
+| 5   | Solar Panel       | 30     | 3000.25 | 4            |
+
+*/
