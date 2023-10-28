98e92648de14da36aaf30806943a5f56663d8b0e
adryanmaikel <adryanmaikel@gmail.com>
2023-10-28 11:40:54 -0300

    pgnd  ID e o nome dos filmes cujo preço for > 2.00

--- 2613 - Filmes em Promoção.sql
+++ 2613 - Filmes em Promoção.sql
@@ -1,3 +1,30 @@
+/* Filmes em Promoção
+Antigamente a locadora fez um evento em que vários filmes estavam em promoção,
+queremos saber que filmes eram esses.
+Seu trabalho para nós ajudar é selecionar o ID e o nome dos filmes cujo preço for
+menor que 2.00.
+
+## Tabela "movies"
+
+| id  | name                         | id_prices |
+| --- | ---------------------------- | --------- |
+| 1   | Batman                       | 3         |
+| 2   | The Battle of the Dark River | 3         |
+| 3   | White Duck                   | 5         |
+| 4   | Breaking Barriers            | 4         |
+| 5   | The Two Hours                | 2         |
+
+## Tabela "prices"
+
+| id  | categorie    | value |
+| --- | -----------  | ----- |
+| 1   | Releases     | 3.50  |
+| 2   | Bronze Seal  | 2.00  |
+| 3   | Silver Seal  | 2.50  |
+| 4   | Gold Seal    | 3.00  |
+| 5   | Promotion    | 1.50  |
+*/
+
 SELECT
   movies.id,
   movies.name
