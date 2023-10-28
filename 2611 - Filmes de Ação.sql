61c1186554d115ef3a7300f939d7d6ca5277d128
adryanmaikel <adryanmaikel@gmail.com>
2023-10-28 11:38:42 -0300

    pegando filmes caso o genero seja 'Action'

--- 2611 - Filmes de Ação.sql
+++ 2611 - Filmes de Ação.sql
@@ -1,7 +1,33 @@
-SELECT 
+/* Filmes de Ação
+Uma Vídeo locadora contratou seus serviços para catalogar os filmes dela. Eles
+precisam que você selecione o código e o nome dos filmes cuja descrição do gênero
+seja 'Action'.
+
+## Tabela "movies"
+
+| id  | name                         | id_genres |
+| --- | ---------------------------- | --------- |
+| 1   | Batman                       | 3         |
+| 2   | The Battle of the Dark River | 3         |
+| 3   | White Duck                   | 1         |
+| 4   | Breaking Barriers            | 4         |
+| 5   | The Two Hours                | 2         |
+
+## Tabela "genres"
+
+| id  | description |
+| --- | ----------- |
+| 1   | Animation   |
+| 2   | Horror      |
+| 3   | Action      |
+| 4   | Drama       |
+| 5   | Comedy      |
+*/
+
+SELECT
   m.id,
   m.name
-FROM 
+FROM
   genres g,
   movies m
 WHERE
