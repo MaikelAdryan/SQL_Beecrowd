aee1a65d51f8f85c7f94cf59a52038fd46166c18
adryanmaikel <adryanmaikel@gmail.com>
2023-10-28 11:43:26 -0300

    pgnd nome e data das locações do mes de setembro

--- 2614 - Locações de Setembro.sql
+++ 2614 - Locações de Setembro.sql
@@ -1,4 +1,32 @@
-SELECT 
+/* Locações de Setembro
+A vídeo locadora está fazendo seu relatório semestral e precisa da sua ajuda.
+Basta você selecionar o nome dos clientes e a data de locação, das locações
+realizadas no mês de setembro de 2016.
+
+## Tabela "customers"
+
+| id  | name                           | street                          | city            |
+| --- | ------------------------------ | ------------------------------- | --------------- |
+| 1   | Giovanna Goncalves Oliveira    | Rua Mato Grosso                 | Canoas          |
+| 2   | Kauã Azevedo Ribeiro           | Travessa Ibiá                   | Uberlândia      |
+| 3   | Rebeca Barbosa Santos          | Rua Observatório Meteorológico  | Salvador        |
+| 4   | Sarah Carvalho Correia         | Rua Antônio Carlos da Silva     | Apucarana       |
+| 5   | João Almeida Lima              | Rua Rio Taiuva                  | Ponta Grossa    |
+| 6   | Diogo Melo Dias                | Rua Duzentos e Cinqüenta        | Várzea Grande   |
+
+## Tabela "rentals"
+
+| id  | rentals_date  | id_customers |
+| --- | ------------- | ------------ |
+| 1   | 2016-09-10    | 3            |
+| 2   | 2016-02-09    | 1            |
+| 3   | 2016-02-08    | 4            |
+| 4   | 2016-02-09    | 2            |
+| 5   | 2016-02-03    | 6            |
+| 6   | 2016-04-04    | 4            |
+*/
+
+SELECT
   c.name,
   r.rentals_date
 FROM
