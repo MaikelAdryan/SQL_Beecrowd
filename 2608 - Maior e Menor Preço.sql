e7d0af842705a512d7290eda56338324019885be
adryanmaikel <adryanmaikel@gmail.com>
2023-10-28 11:30:50 -0300

    selecionando o produto com o menor e maior preço

--- 2608 - Maior e Menor Preço.sql
+++ 2608 - Maior e Menor Preço.sql
@@ -1,1 +1,16 @@
+/* Maior e Menor Preço
+O setor financeiro da nossa empresa, está querendo saber os menores e maiores valores dos produtos, que vendemos.
+Para isso exiba somente o maior e o menor preço da tabela produtos.
+
+## Tabela "products"
+
+| id  | name                | amount | price  |
+| --- | ------------------- | ------ | -------|
+| 1   | Two-doors wardrobe  | 100    | 800.00 |
+| 2   | Dining table        | 1000   | 560.00 |
+| 3   | Towel holder        | 10000  | 25.50  |
+| 4   | Computer desk       | 350    | 320.50 |
+| 5   | Chair               | 3000   | 210.64 |
+| 6   | Single bed          | 750    | 460.00 |
+*/
 SELECT MAX(price), MIN(price) FROM products
